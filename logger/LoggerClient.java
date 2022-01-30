54994057ae330d8b7833d234753bb708a588be99
Amit Thakur <amit.thakur17@vit.edu>
2022-01-30 19:45:10 +0530

    Completed the initial code for the logger

--- logger/LoggerClient.java
+++ logger/LoggerClient.java
@@ -1,7 +1,6 @@
 package logger;
 
 import java.util.*;
-import java.io.*;
 
 class Process {
     private final String processID;
@@ -30,11 +29,76 @@ class Process {
         return this.endTime;
     }
 }
+class Logger implements loggerInterface {
+    Map<String, Process> processes = new HashMap<>();
+    TreeMap<Long, Process> queue = new TreeMap<>();
 
+    public Logger() {
+        this.processes = new HashMap<>();
+        this.queue = new TreeMap<>();
+    }
+
+    public boolean startProcess(String processID) {
+        System.out.println("Starting" + " " + processID);
+        final long now = System.currentTimeMillis();
+        final Process process = new Process(processID, now);
+        processes.put(processID, process);
+        queue.put(now, process);
+
+        return true;
+    }
+
+    public boolean endProcess(String processID) {
+        final long now = System.currentTimeMillis();
+        processes.get(processID).setEndTime(now);
+        return true;
+    }
+
+    public void pollProcess() {
+        if (queue.isEmpty()) {
+            System.out.println("No Processes");
+            return;
+        }
+
+        final Process process = queue.firstEntry().getValue();
+
+        if (process.getEndTime() != -1) {
+            System.out.println("The given process with the process ID started at " + process.getProcessID() + " " + process.getStartTime() + " and it ended at " + process.getEndTime());
+            queue.pollFirstEntry();
+            processes.remove(process.getProcessID());
+        }
+        else {
+            System.out.println("No End Time for the " + process.getProcessID());
+        }
+    }
+}
 
 
 class LoggerClient {
     public static void main(String[] args) {
+        final Logger logger = new Logger();
+        System.out.println("======= Starting the Process Logger ===========");
 
+        // logger.startProcess("1");
+        // logger.pollProcess();
+        // logger.startProcess("2");
+        // logger.pollProcess();
+        // logger.startProcess("3");
+        logger.startProcess("1");
+        logger.pollProcess();
+        logger.startProcess("3");
+        logger.pollProcess();
+        logger.endProcess("1");
+        logger.pollProcess();
+        logger.startProcess("2");
+        logger.pollProcess();
+        logger.endProcess("2");
+        logger.pollProcess();
+        logger.endProcess("3");
+        logger.pollProcess();
+        logger.pollProcess();
+        logger.pollProcess();
+
+        System.out.println("============ Completed the Logging ======================");
     }
 }
--- logger/loggerInterface.java
+++ logger/loggerInterface.java
@@ -2,6 +2,6 @@ package logger;
 
 public interface loggerInterface {
     public boolean startProcess(String processID);
-    public boolean endProcess(String processID, long time);
+    public boolean endProcess(String processID);
     public void pollProcess();
 }
